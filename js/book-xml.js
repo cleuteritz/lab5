917f2a16ff23e90ef2edc779726f432e927442c5
cleuteritz <cleuteritz@uri.edu>
2024-02-28 23:52:30 +0000

    book-xml file

---
+++ js/book-xml.js
@@ -0,0 +1,54 @@
+function changeImageOpacity() {
+    const images = document.querySelectorAll('img');
+    images.forEach((image) => {
+        image.style.opacity = 0.5;
+    });
+}
+
+function loadXML(filePath, subElementIndex) {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(xhr.responseText, 'text/xml');
+            
+            // Process the XML data and add it to the DOM
+            const detailsDiv = document.getElementById('details');
+            detailsDiv.innerHTML = ''; // Clear the existing content
+
+            const subElements = xmlDoc.querySelectorAll('*');
+            const selectedSubElement = subElements[subElementIndex];
+
+            // Check if the selected sub-element exists
+            if (selectedSubElement) {
+                // Add the selected sub-element to the detailsDiv
+                detailsDiv.appendChild(selectedSubElement.cloneNode(true));
+            }
+        }
+    };
+    xhr.open('GET', filePath, true);
+    xhr.send();
+}
+
+function handleImageClick(imageElement, dataFile, subElementIndex) {
+    loadXML(dataFile, subElementIndex);
+    changeImageOpacity();
+    imageElement.style.opacity = 1;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const donQuixoteImage = document.querySelector('#don-quixote-img');
+    donQuixoteImage.addEventListener('click', function() {
+        handleImageClick(donQuixoteImage, 'don-quixote-data.xml', 0);
+    });
+
+    const taleOfTwoCitiesImage = document.querySelector('#two-cities-img');
+    taleOfTwoCitiesImage.addEventListener('click', function() {
+        handleImageClick(taleOfTwoCitiesImage, 'tale-of-two-cities-data.xml', 1);
+    });
+
+    const lordOfTheRingsImage = document.querySelector('#lotr-img');
+    lordOfTheRingsImage.addEventListener('click', function() {
+        handleImageClick(lordOfTheRingsImage, 'lord-of-the-rings-data.xml', 2);
+    });
+});
