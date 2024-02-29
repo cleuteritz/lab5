1f7881cff2ea1298399c07eeca1fa4e0e8a313c7
cleuteritz <cleuteritz@uri.edu>
2024-02-29 00:52:49 +0000

    attempt to fix

--- js/book-json.js
+++ js/book-json.js
@@ -1,24 +1,29 @@
 function loadJSON(filePath, objectIndex) {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const jsonData = JSON.parse(xhr.responseText);
-            let newHTML = '';
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const jsonData = JSON.parse(xhr.responseText);
+                let newHTML = '';
 
-            jsonData.forEach((item, index) => {
-                if (index === objectIndex) {
-                    // Assuming each item has properties like 'title', 'author', etc.
-                    newHTML += `<p>Title: ${item.title}</p>`;
-                    newHTML += `<p>Author: ${item.author}</p>`;
-                    newHTML += `<p>Sold: ${item.sold}</p>`;
-                    newHTML += `<p>Description: ${item.description}</p>`;
-                }
-            });
+                jsonData.forEach((item, index) => {
+                    if (index === objectIndex) {
+                        // Assuming each item has properties like 'title', 'author', etc.
+                        newHTML += `<p>Title: ${item.title}</p>`;
+                        newHTML += `<p>Author: ${item.author}</p>`;
+                        newHTML += `<p>Sold: ${item.sold}</p>`;
+                        newHTML += `<p>Description: ${item.description}</p>`;
+                    }
+                });
 
-            const detailsDiv = document.getElementById('details');
-            detailsDiv.innerHTML = newHTML;
+                const detailsDiv = document.getElementById('details');
+                detailsDiv.innerHTML = newHTML;
+            } else {
+                console.error(`Failed to load JSON. Status: ${xhr.status}`);
+            }
         }
     };
+    // Update the file path to point to the 'data' subdirectory
     xhr.open('GET', 'data/' + filePath, true);
     xhr.send();
 }
@@ -26,21 +31,22 @@ function loadJSON(filePath, objectIndex) {
 document.addEventListener('DOMContentLoaded', function() {
     const donQuixoteImage = document.querySelector('#don-quixote-img');
     donQuixoteImage.addEventListener('click', function() {
-        loadJSON('cervantes-data.json', 0);
+        loadJSON('book-data.json', 0);
         changeImageOpacity();
         donQuixoteImage.style.opacity = 1;
     });
 
+
     const taleOfTwoCitiesImage = document.querySelector('#two-cities-img');
     taleOfTwoCitiesImage.addEventListener('click', function() {
-        loadJSON('dickens-data.json', 0);
+        loadJSON('book-data.json', 1);
         changeImageOpacity();
         taleOfTwoCitiesImage.style.opacity = 1;
     });
 
     const lordOfTheRingsImage = document.querySelector('#lotr-img');
     lordOfTheRingsImage.addEventListener('click', function() {
-        loadJSON('tolkein-data.json', 0);
+        loadJSON('book-data.json', 2);
         changeImageOpacity();
         lordOfTheRingsImage.style.opacity = 1;
     });
